4d1282436715821a493be09c71a73932c22945dc
quentin deleglise <deleglise.quentin@hotmail.fr>
2020-06-30 20:44:10 +0200

    ajout du form modifications params

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -69,6 +69,6 @@ class BooksController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def book_params
-      params.require(:book).permit(:title, :description)
+      params.require(:book).permit(:title, :description, :picture, :amazon_url, :author, :language, :category)
     end
 end
